51d3715fbbdc53b5e3e90d57c5db31578ef70c8c
Alisasmith <Alisasmith@126.com>
2017-05-18 10:26:14 +0800

    Initial commit

---
+++ FirstDay_Study/FirstDemo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: jszc
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: 1558819350@qq.com
+@software: garner
+@file: FirstDemo.py
+@time: 2017/5/18 9:19
+@desc:
+'''
